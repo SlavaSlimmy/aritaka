18ce06c2e6a4d674868b40cda7d575f6eaa6cccf
Slava Egorov <slava.egorov@gmail.com>
2016-02-19 19:23:20 +0300

    added gulp tasks to minify CSS and JS

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,12 @@ var gulp = require('gulp'),
     less = require('gulp-less'),
     path = require('path'),
     sourcemaps = require('gulp-sourcemaps'),
-    spritesmith = require('gulp.spritesmith');
-
+    spritesmith = require('gulp.spritesmith'),
+    cssnano = require('gulp-cssnano'),
+    uglify = require('gulp-uglify'),
+    rename = require("gulp-rename"),
+    concat = require('gulp-concat'),
+    clean = require('gulp-clean');
 
 // less
 gulp.task('less', function () {
@@ -28,6 +32,28 @@ gulp.task('sprite', function () {
         cssTemplate: 'images/sprite-images/sprite.css.handlebars'
     }));
     return spriteData.pipe(gulp.dest('images'));
+});
+
+// minify CSS
+gulp.task('mincss', function() {
+    return gulp.src('css/main.css')
+        .pipe(cssnano())
+        .pipe(rename("main.min.css"))
+        .pipe(gulp.dest('css'));
+});
+
+// compress JS
+gulp.task('minjs', ['clean-js'], function() {
+    return gulp.src('js/*.js')
+        .pipe(concat('app.js'))
+        .pipe(uglify())
+        .pipe(rename("app.min.js"))
+        .pipe(gulp.dest('js'));
+});
+
+gulp.task('clean-js', function () {
+    return gulp.src('js/app.min.js', {read: false})
+        .pipe(clean());
 });
 
 // watch
